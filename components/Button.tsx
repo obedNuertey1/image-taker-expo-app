4bb2bcfd80bab9e34b0259a140b81d49d381ca9e
Obed Nuertey <onuertey1997@gmail.com>
2024-07-08 15:11:04 +0000

    patch(Button.tsx): Added onPress props to function object desctructuring and added onPress to onPress props on pressable

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,12 +4,12 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 const [View, Pressable, Text] = [styled(RnView), styled(RnPressable), styled(RnText)]
 
-export default function({label, theme}:any){
+export default function({label, theme, onPress}:any){
     if(theme === "primary"){
         return (
             <View className='border-4 border-[#ffd33d] rounded-[18px] w-80 h-[68px] mx-5 items-center justify-center p-[3px]'>
                 <Pressable className='bg-white rounded-[10px] w-full h-full items-center justify-center flex-row'
-                    onPress={()=>alert('You pressed a button.')}
+                    onPress={onPress}
                 >
                     <View className='pr-[8px] inline'>
                         <FontAwesome name="picture-o" size={18} color={"#25292e"}/>
