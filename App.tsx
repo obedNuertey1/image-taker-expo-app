abb0839ebabef48d36d783511ee7d52ea948262e
Obed Nuertey <onuertey1997@gmail.com>
2024-07-08 15:05:00 +0000

    patch(App.tsx): added code for picking image

--- App.tsx
+++ App.tsx
@@ -4,8 +4,22 @@ import { View } from "react-native";
 import PlaceHolderImage from "./assets/images/background-image.png";
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App(){
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true, quality: 1
+    })
+
+    if(!result.canceled){
+      console.log(result);
+    }else{
+      alert('You did not select any image.')
+    }
+  };
+
+  
   return (
     <View className="flex-[1] bg-[#25292e] items-center justify-center">
       <View className="flex-[1] pt-[58px]">
