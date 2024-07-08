8537f36b45092f74e403095b86339b7e7b8b913c
Obed Nuertey <onuertey1997@gmail.com>
2024-07-08 14:19:02 +0000

    feat(Button.tsx, App.tsx, package.json, tailwind.config.ts): added code to implement fist screen

--- App.tsx
+++ App.tsx
@@ -3,12 +3,17 @@ import { View } from "react-native";
 // @ts-ignore
 import PlaceHolderImage from "./assets/images/background-image.png";
 import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
 
 export default function App(){
   return (
-    <View className="flex flex-1 bg-[#25292e] items-center justify-center">
-      <View className="flex flex-1 pt-[58px]">
+    <View className="flex-[1] bg-[#25292e] items-center justify-center">
+      <View className="flex-[1] pt-[58px]">
         <ImageViewer placeholderImage={PlaceHolderImage} />
+      </View>
+      <View className="flex-[1/3] flex-col h-1/3 items-center justify-center">
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
       <StatusBar style="light" />
     </View>
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,32 @@
-import {View, Pressable, Text} from 'react-native';
+import {View as RnView, Pressable as RnPressable, Text as RnText} from 'react-native';
+import { styled } from 'nativewind';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function({label}:any){
+const [View, Pressable, Text] = [styled(RnView), styled(RnPressable), styled(RnText)]
+
+export default function({label, theme}:any){
+    if(theme === "primary"){
+        return (
+            <View className='border-4 border-[#ffd33d] rounded-[18px] w-80 h-[68px] mx-5 items-center justify-center p-[3px]'>
+                <Pressable className='bg-white rounded-[10px] w-full h-full items-center justify-center flex-row'
+                    onPress={()=>alert('You pressed a button.')}
+                >
+                    <View className='pr-[8px] inline'>
+                        <FontAwesome name="picture-o" size={18} color={"#25292e"}/>
+                    </View>
+                    <Text
+                        className='text-[#25292e] text-base'
+                    >{label}</Text>
+                </Pressable>
+            </View>
+        );
+    }
     return (
-        <View className='w-80 h-[68px] '>
-            <Pressable className='rounded-[10px] w-[100%] itemx-center justify-center flex flex-row'
+        <View className='w-80 h-[68px] mx-5 items-center justify-center p-[3px]'>
+            <Pressable className='rounded-[10px] w-full h-[100%] items-center justify-center flex-row'
             onPress={()=>alert('You pressed a button.')}
             >
-                <Text className='text-white text-[16px] mx-5 items-center justify-center p-[3px]'>{label}</Text>
+                <Text className='text-white text-base'>{label}</Text>
             </Pressable>
         </View>
     );
